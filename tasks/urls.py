647bcde95b0d99f3b16dd8b3526a236e125801f1
adonai76 <21240785@leon.tecnm.mx>
2025-09-03 09:08:26 -0600

    documentacion

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import AlumnoViewSet
+from rest_framework.documentation import include_docs_urls
+
 
 router = routers.DefaultRouter()
 router.register(r'alumnos', AlumnoViewSet, 'alumnos')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('docs/', include_docs_urls(title='API de Gestión de Alumnos'))
 ]
